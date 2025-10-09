598e2948badbae47eb6230b60a3a1bd3d7138eff
Lindsay Hall <lindsayh171@gmail.com>
2025-10-09 15:46:28 -0400

    Please work

---
+++ clerkshipApp/UpDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpDownApp.swift
+//  UpDown
+//
+//  Created by user285542 on 10/5/25.
+//
+
+import SwiftUI
+
+@main
+struct UpDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ clerkshipAppTests/UpDownTests.swift
@@ -0,0 +1,17 @@
+//
+//  UpDownTests.swift
+//  UpDownTests
+//
+//  Created by user285542 on 10/5/25.
+//
+
+import Testing
+@testable import UpDown
+
+struct UpDownTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
