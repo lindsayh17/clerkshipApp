683aea2e0c57d39a16dd53de4c17ff02d2adc569
hannah <hdeyst@uvm.edu>
2025-10-09 16:00:21 -0400

    new files new project new me

---
+++ clerkshipApp/clerkshipApp/clerkshipAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  clerkshipAppApp.swift
+//  clerkshipApp
+//
+//  Created by Hannah Deyst on 10/9/25.
+//
+
+import SwiftUI
+
+@main
+struct clerkshipAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ clerkshipApp/clerkshipAppTests/clerkshipAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  clerkshipAppTests.swift
+//  clerkshipAppTests
+//
+//  Created by Hannah Deyst on 10/9/25.
+//
+
+import Testing
+@testable import clerkshipApp
+
+struct clerkshipAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
