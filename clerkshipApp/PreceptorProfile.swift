8d25c5e550893b9b8070d1c3ae0a5fbe4edcda2b
Lindsay Hall <lhall11@uvm.edu>
2025-10-23 15:06:20 -0400

    Used NavView

--- clerkshipApp/PreceptorProfile.swift
+++ clerkshipApp/PreceptorProfile.swift
@@ -44,44 +44,7 @@ struct PreceptorProfile: View {
                         .padding()
                     }
                     
-                    // Bottom white transparent nav bar
-                    ZStack {
-                        // Color + rounded corners
-                        Color.white
-                            // Transparency
-                            .opacity(0.6)
-                            .cornerRadius(35)
-                            .frame(height: 90)
-                            // Side spacing
-                            .padding(.horizontal, 25)
-                            // Bottom spacing
-                            .padding(.bottom, 10)
-                        
-                        // Feedback & Profile buttons
-                        HStack(spacing: 60) {
-                            // Feedback button
-                            VStack {
-                                Circle()
-                                    // Olive green
-                                    .fill(Color(red: 0.68, green: 0.69, blue: 0.53))
-                                    .frame(width: 50, height: 50)
-                                Text("Feedback")
-                                    .foregroundColor(.black)
-                                    .padding(.top, 4)
-                            }
-                            
-                            // Profile button
-                            VStack {
-                                Circle()
-                                    // Olive green
-                                    .fill(Color(red: 0.68, green: 0.69, blue: 0.53))
-                                    .frame(width: 50, height: 50)
-                                Text("Profile")
-                                    .foregroundColor(.black)
-                                    .padding(.top, 4)
-                            }
-                        }
-                    }
+                    NavView()
                 }
             }
         }
