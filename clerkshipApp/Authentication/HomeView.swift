be3fcd0287f9c6fc206f0a5ed5b368a467dd7cec
Lindsay Hall <lhall11@uvm.edu>
2025-10-27 14:39:04 -0400

    Rough initial signin and home screen pages

---
+++ clerkshipApp/Authentication/HomeView.swift
@@ -0,0 +1,16 @@
+//
+//  LoginView.swift
+//  clerkshipApp
+//
+//  Created by Lindsay on 10/27/25.
+//
+
+import SwiftUI
+
+struct HomeView: View{
+    var body: some View{
+        VStack{
+            
+        }
+    }
+}
