fa62d4f7bbe3edf0f80ffdcb70dc31b929652832
hannah <hdeyst@uvm.edu>
2025-10-28 14:42:59 -0400

    renaming and very preliminary work on login view

--- clerkshipApp/FirebaseService.swift
+++ clerkshipApp/FirebaseService.swift
@@ -10,14 +10,28 @@ class FirebaseService: ObservableObject {
 //    @Published private(set) var forms: [EvaluationForm]
 //    @Published var downloadSuccessful = false
     let formCollectionName = "Forms"
+    
+    // TODO: user collection values
+    let userCollectionName = " "
     var db: Firestore!
     
     init() {
         db = Firestore.firestore()
     }
 
+    // function to fetch form info from firebase
     func fetchForms() async throws {
         let querySnapshot = try await db.collection(formCollectionName).getDocuments()
+        for document in querySnapshot.documents {
+            let data = document.data()
+            print(data)
+        }
+    }
+    
+    // function to fetch user info from firebase
+    // TODO: right now fetchUsers is defunct b/c no collection like that so far
+    func fetchUsers() async throws {
+        let querySnapshot = try await db.collection(userCollectionName).getDocuments()
         for document in querySnapshot.documents {
             let data = document.data()
             print(data)
