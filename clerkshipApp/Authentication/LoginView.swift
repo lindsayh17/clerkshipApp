60dd2d7793e01af7f11e62eaa18e5ff2806c84f9
hannah <hdeyst@uvm.edu>
2025-10-28 22:43:54 -0400

    some work on beautify-ing the buttons and login

--- clerkshipApp/Authentication/LoginView.swift
+++ clerkshipApp/Authentication/LoginView.swift
@@ -18,7 +18,7 @@ struct LoginView: View {
     
     // Colors
     private let backgroundColor = Color("BackgroundColor")
-    private let buttonColor = Color("ButtonColor")
+//    private let buttonColor = Color("ButtonColor")
     
     // Firebase Download
     func signin() {
@@ -30,25 +30,55 @@ struct LoginView: View {
     }
     
     var body: some View {
-        VStack {
+        ZStack {
             // Color fills the entire screen
-            // backgroundColor.ignoresSafeArea()
-            
-            // TODO: email validation
-            // TODO: remove autocapitilization
-            TextField("Email...", text: $email)
-                .padding()
-                .cornerRadius(10)
-                .background(Color.gray.opacity(0.4))
-            SecureField("Password...", text: $password)
-                .padding()
-                .cornerRadius(10)
-                .background(Color.gray.opacity(0.4))
-            
-            // TODO: add navigation if sign in successful
-            Button(action: signin, label: {
-                Text("Sign In")
-            })
+            backgroundColor.ignoresSafeArea()
+            VStack {
+                Text("UVM OBGYN Clerkship App")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .bold()
+                    .padding()
+                
+                // TODO: email validation
+                // TODO: remove autocapitilization
+                TextField("Email...", text: $email)
+                    .padding()
+                    .cornerRadius(10)
+                    .background(Color.gray.opacity(0.4))
+                
+                SecureField("Password...", text: $password)
+                    .padding()
+                    .cornerRadius(10)
+                    .background(Color.gray.opacity(0.4))
+                
+                // TODO: add navigation if sign in successful
+                BigButtonView(buttonText: "Log In", action: signin, foregroundColor: .white, backgroundColor: backgroundColor)
+                    
+            }
+            .padding()
+        }
+    }
+}
+
+struct BigButtonView: View {
+    var buttonText: String = ""
+    var action: () -> Void
+    var width: CGFloat = 300
+    var height: CGFloat = 60
+    var foregroundColor = Color.white
+    var backgroundColor = Color.accentColor
+    
+    var body: some View {
+        Button(action: action) {
+            Text(buttonText)
+                .font(.title)
+                .fontWeight(.semibold)
+                .frame(width: width, height: height)
+                .border(.white, width: 5)
+                .foregroundColor(foregroundColor) .background(backgroundColor) .overlay(RoundedRectangle(cornerRadius: 5)
+                          .stroke(Color.black, lineWidth: 2))
+                        .cornerRadius(5)
         }
         .padding()
     }
