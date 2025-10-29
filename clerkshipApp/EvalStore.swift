fe7784825622f4a8c93e07ceb5cf371d3f05a6eb
Lindsay Hall <lhall11@uvm.edu>
2025-10-29 16:13:55 -0400

    send form info to database

--- clerkshipApp/EvalStore.swift
+++ clerkshipApp/EvalStore.swift
@@ -9,19 +9,13 @@
 import Firebase
 
 class EvalStore: ObservableObject {
-  @Published var allForms: [Form]
     private var db = Firestore.firestore()
-    
-    init(allForms: [Form]) {
-        self.allForms = allForms
-    }
     
     func add(form: Form) {
         do {
-          // 2. Add the form to the Firestore "forms" collection
-            _ = try db.collection("forms").addDocument(from: form)
-        } catch {
-            print("Error adding form to Firestore: \(error)")
+          try db.collection("Forms").document("2").setData(from: form)
+        } catch let error {
+          print("Error writing city to Firestore: \(error)")
         }
     }
   
