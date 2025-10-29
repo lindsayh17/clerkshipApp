af0cbd8a3a7e913ec147d03ff828e2712bae19ff
hannah <hdeyst@uvm.edu>
2025-10-29 15:55:01 -0400

    formatting

--- clerkshipApp/BigButtonView.swift
+++ clerkshipApp/BigButtonView.swift
@@ -1,5 +1,10 @@
+//
+//  BigButtonView.swift
+//  clerkshipApp
+//
 
 import SwiftUI
+
 struct BigButtonView: View {
     var buttonText: String = ""
     var action: () -> Void
@@ -8,45 +13,19 @@ struct BigButtonView: View {
     var foregroundColor = Color.white
     var backgroundColor = Color.accentColor
 
-
-
     var body: some View {
-
-
         Button(action: action) {
-
-
             Text(buttonText)
-
-
                 .font(.title)
-
-
                 .fontWeight(.semibold)
-
-
                 .frame(width: width, height: height)
-
-
                 .border(.white, width: 5)
-
-
-                .foregroundColor(foregroundColor) .background(backgroundColor) .overlay(RoundedRectangle(cornerRadius: 5)
-
-
+                .foregroundColor(foregroundColor)
+                .background(backgroundColor)
+                .overlay(RoundedRectangle(cornerRadius: 5)
                           .stroke(Color.black, lineWidth: 2))
-
-
                         .cornerRadius(5)
-
-
         }
-
-
         .padding()
-
-
     }
-
-
 }
