fe5070425229825c7d7f81f9308038c22c8156da
Lindsay Hall <lhall11@uvm.edu>
2025-11-01 17:55:04 -0400

    Initializer

--- clerkshipApp/Evaluation/Evaluation.swift
+++ clerkshipApp/Evaluation/Evaluation.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 
 struct Evaluation: Identifiable, Codable {
-    var id: UUID
+    var id = UUID()
     var formId: String                // Link to form type
     var preceptorId: String           // ID of preceptor filling out the form
     var studentId: String             // ID of student being evaluated
     var responses: [Response]
     var submittedAt: Date
+    
+    init(id: UUID, formId: String, preceptorId: String, studentId: String, responses: [Response], submittedAt: Date) {
+        self.formId = formId
+        self.preceptorId = preceptorId
+        self.studentId = studentId
+        self.responses = responses
+        self.submittedAt = submittedAt
+    }
 }
 
 struct Response: Identifiable, Codable {
