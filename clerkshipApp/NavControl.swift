d935904df9d415203b097f23ad3921ea95a76e1e
Lindsay Hall <lhall11@uvm.edu>
2025-11-03 17:44:22 -0500

    Made nav control object

---
+++ clerkshipApp/NavControl.swift
@@ -0,0 +1,12 @@
+//
+//  NavControl.swift
+//  clerkshipApp
+//
+//  Created by Lindsay on 11/3/25.
+//
+
+import SwiftUI
+
+class NavControl: ObservableObject{
+    @Published var showRoot: Bool = false
+}
