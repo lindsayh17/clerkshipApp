79237c3c9451e037b8ac63e77c63e124465beb1c
miacorcoran <s2587166@ed.ac.uk>
2025-11-11 19:27:50 -0500

    Schedule View

--- clerkshipApp/ScheduleView.swift
+++ clerkshipApp/ScheduleView.swift
@@ -21,21 +21,28 @@ struct ScheduleView: View {
     private let schedule: [ScheduleData] = [
         ScheduleData(title: "Monday, 10/13",
                       descriptions:  [
-                        "All students. Simulation Center - Rowell 2nd Floor.",
-                        "7:00am - 8:00am: Introduction & Pre-Rotation Knowledge Test (bring a computer).",
-                        "8:00am - 12:00pm: Simulation Activities & Hospital Tour."
+                        "**All students**",
+                        "*Simulation Center - Rowell 2nd Floor*",
+                        "7:00am - 8:00am: Introduction & Pre-Rotation Knowledge Test (bring a computer)\n8:00am - 12:00pm: Simulation Activities & Hospital Tour",
+                        "**All students**",
+                        "*Meded 302/303*",
+                        "Hot lunch will be served - please email Sara Tourville with any food/allergy considerations.",
+                        "12:00pm: Lunch & Clerkship Information\n12:30pm - 1:30pm: Intrapartum FHR Interpretation Teaching Session\n1:30pm - 1:45pm: Break\n1:45pm - 2:30pm: Labor Support and Trauma Informed Care Session\n2:30pm - 3:30pm: Family Planning on the OBGyn Clerkship",
+                        "**L. Amaio, J. Bates, A. Gorbacheva, J. Reigle, B. Sebuufu**",
+                        "*Given Assessment Center*",
+                        "5:45pm - 8:45pm: Pelvic & Breast Refresher"
                     ]),
         ScheduleData(title: "Tuesday 10/14",
                       descriptions:  [
-                        "All students. Simulation Center - Rowell 2nd Floor.",
-                        "7:00am - 8:00am: Introduction & Pre-Rotation Knowledge Test (bring a computer).",
-                        "8:00am - 12:00pm: Simulation Activities & Hospital Tour."
+                        "**K. Blau, B. Canova, J. Hurley, R. Marawala**",
+                        "*Given Assessment Center*",
+                        "5:45pm - 8:15pm: Pelvic & Breast Refresher"
                     ]),
         ScheduleData(title: "Wednesday 10/15",
                       descriptions:  [
-                        "All students. Simulation Center - Rowell 2nd Floor.",
-                        "7:00am - 8:00am: Introduction & Pre-Rotation Knowledge Test (bring a computer).",
-                        "8:00am - 12:00pm: Simulation Activities & Hospital Tour."
+                        "**C. Bauman, J. Du, M. Lapointe-Gagner, S. Tukel**",
+                        "*Given Assessment Center*",
+                        "5:45pm - 8:15pm: Pelvic & Breast Refresher"
                     ])
     ]
     
@@ -66,7 +73,7 @@ struct ScheduleView: View {
                                             .foregroundColor(.white)
                                         
                                         ForEach(item.descriptions, id: \.self) { desc in
-                                             Text(desc)
+                                            Text(LocalizedStringKey(desc))
                                                  .font(.subheadline)
                                                  .foregroundColor(.white.opacity(0.8))
                                                  .padding(.leading, 8)
