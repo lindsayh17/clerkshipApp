7f0a9202364264fb1e906980d77df325cfc03d7e
miacorcoran <s2587166@ed.ac.uk>
2025-11-19 17:48:57 -0500

    Home view changed for preceptors

--- clerkshipApp/HomeView.swift
+++ clerkshipApp/HomeView.swift
@@ -43,6 +43,72 @@ struct HomeView: View {
             if currUser.user?.access == .admin {
                 AdminDashboardView()
                 
+            } else if currUser.user?.access == .preceptor {
+                // Preceptor view inside HomeView
+                ZStack {
+                    backgroundColor.ignoresSafeArea()
+                    VStack(spacing: 0) {
+                        ScrollView {
+                            VStack(alignment: .leading, spacing: 40) {
+                                switch currentView {
+                                case .home:
+                                    VStack(alignment: .leading, spacing: 4) {
+                                        Text("Welcome, \(currUser.user?.firstName ?? "Preceptor")")
+                                            .font(.title)
+                                            .fontWeight(.bold)
+                                            .foregroundColor(.white)
+                                    }
+                                    Spacer()
+                                    
+                                case .resources:
+                                    ResourcesView()
+                                    
+                                case .search:
+                                    SearchView()
+                                    
+                                case .profile:
+                                    ProfileView()
+                                    
+                                case .users:
+                                    SearchView()
+                                    
+                                case .eval:
+                                    EmptyView() 
+                                }
+                            }
+                            .padding()
+                        }
+                        .padding(.horizontal)
+                        
+                        // Bottom Navigation
+                        NavTab(currentTab: $currentView)
+                            .onChange(of: currentView) { newTab in
+                                if newTab == .eval {
+                                    showEvalFromTab = true
+                                    currentView = .home
+                                }
+                            }
+                    }
+                }
+                .navigationBarBackButtonHidden()
+                .navigationDestination(isPresented: $navControl.showEvalChoice) {
+                    SearchView() // or another view if needed
+                }
+                .navigationDestination(isPresented: $showEvalFromTab) {
+                    if let curr = currUser.user {
+                        FormChoiceView(currStudent: curr) // probably preceptors won't use this
+                    }
+                }
+                .navigationDestination(isPresented: $navControl.showOrientation) {
+                    OrientationView()
+                }
+                .navigationDestination(isPresented: $navControl.showQuickFacts) {
+                    QuickFactsView()
+                }
+                .navigationDestination(isPresented: $navControl.showRequirements) {
+                    ClerkshipRequirementsView()
+                }
+
             } else {
                 // Otherwise show normal app content
                 ZStack {
