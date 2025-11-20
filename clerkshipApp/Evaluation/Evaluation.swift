a0abc9431ac2b449ccbb9699b6d336b7c9241ca3
Lindsay Hall <lhall11@uvm.edu>
2025-11-20 11:05:43 -0500

    Form type instead of form id in firebase

--- clerkshipApp/Evaluation/Evaluation.swift
+++ clerkshipApp/Evaluation/Evaluation.swift
@@ -6,23 +6,23 @@ import SwiftUI
 // a completed form
 struct Evaluation: Identifiable, Codable {
     var id = UUID()
-    var formId: String             // Link to form type
+    var formType: String             // Link to form type
     var preceptorId: String        // ID of preceptor filling out form
     var studentId: String          // ID of student being evaluated
     var responses: [String: String]
     var submittedAt: Date
     var notes = ""
     
-    init(formId: String, preceptorId: String, studentId: String, responses: [String: String], submittedAt: Date) {
-        self.formId = formId
+    init(formType: String, preceptorId: String, studentId: String, responses: [String: String], submittedAt: Date) {
+        self.formType = formType
         self.preceptorId = preceptorId
         self.studentId = studentId
         self.responses = responses
         self.submittedAt = submittedAt
     }
     
-    init(formId: String, preceptorId: String, studentId: String, responses: [String: String], submittedAt: Date, notes: String) {
-        self.formId = formId
+    init(formType: String, preceptorId: String, studentId: String, responses: [String: String], submittedAt: Date, notes: String) {
+        self.formType = formType
         self.preceptorId = preceptorId
         self.studentId = studentId
         self.responses = responses
